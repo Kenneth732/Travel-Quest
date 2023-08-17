e4f24cf7df4dc060917d65de9e678994abbacd6a
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-08-17 16:19:33 +0300

    about

--- src/App.js
+++ src/App.js
@@ -1,44 +1,57 @@
-import React from 'react'
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import Logo from './Components/assets/logo.png'
+import Logo from './Components/assets/logo.png';
 import './App.css';
 import About from './Components/About';
 import Contact from './Components/Contact';
 import Home from './Components/Home';
 
 function App() {
+
   return (
     <div className="App">
       <Router>
-        <nav>
-          <div className='Nav__Bar'>
-            <div className='Nav__Logo'>
-              <img src={Logo} alt='logo' className='Logo' />
-              {/* <a href='/'>Travel Quest</a> */}
-            </div>
+        <header className='header'>
+          <nav>
+            <div className='Nav__Bar'>
+              <div className='Nav__Logo'>
+                <img src={Logo} alt='logo' className='Logo' />
+              </div>
 
-            <ul className='Nav__Links'>
-              <li className='link'><Link className='links' to="/">HOME</Link></li>
-              <li className='link'><Link className='links' to="/">ANOUT</Link></li>
-              <li className='link'><Link className='links' to="/">CONTACT</Link></li>
-              {/* <li className='link'><Link className='links' to="/">HOME</Link></li> */}
-              <li className='link search'>
-                <span>
-                <i class="bi bi-search"></i>
-                </span>
-              </li>
-            </ul>
+              <ul className='Nav__Links'>
+                <li className='link'><Link className='links' to="/">HOME</Link></li>
+                <li className='link'><Link className='links' to="/about">ABOUT</Link></li>
+                <li className='link'><Link className='links' to="/contact">CONTACT</Link></li>
+                <li className='link search'>
+                  <span>
+                    <i className="bi bi-search"></i>
+                  </span>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <div className="section__container header__container">
+            <h1>Embark on a Journey Beyond: Explore with Travel Quest</h1>
+            <h4>Unveil the Marvels of Our World and Beyond with Travel Quest</h4>
+            <button className="btn">
+              Explore More <i className="bi bi-arrow-right-circle-fill"></i>
+            </button>
           </div>
-        </nav>
+        </header>
 
         <Routes>
-           <Route path='/' elem={<Home />} />
-           <Route path='/' elem={<About />} />
-           <Route path='/' elem={<Contact />} />
-           </Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+
+
+
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,55 @@
-import React from 'react'
+
+
+
+
+import React from 'react';
+import './Home.css';
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <div className=''>
+            <section className="about" id="about">
+                <div className="section__container about__container">
+                    <div className="about__content">
+                        <h2 className="section__header">About us</h2>
+                        <p className="section__subheader">
+                        Dive into the heart-pounding world of exploration with Travel Quest! We are not just a travel website; we're your passport to uncharted territories, adrenaline-pumping escapades, and breathtaking discoveries. Feel the rush as you traverse vibrant cities, conquer towering peaks, and navigate untamed wilderness.
+
+                        At Travel Quest, we're obsessed with delivering you the most extraordinary experiences. Our team of intrepid adventurers scours the globe to unearth hidden gems, insider secrets, and jaw-dropping destinations that will leave you in awe. Get ready to embark on journeys that redefine your limits and redefine your world.
+
+                        {/* With curated itineraries, expert advice, and an unwavering commitment to authenticity, Travel Quest is your partner in creating memories that will last a lifetime. Our mission? To ignite the fire of wanderlust within you, pushing you to explore further, dream bigger, and live life to the fullest. */}
+{/* 
+                        Adventure beckons – are you ready to answer the call? Join us on this exhilarating ride as we take you on a quest that will redefine the way you experience the world. */}
+                    </p>
+                        <div className="about__flex">
+                            <div className="about__card">
+                                <h4>268</h4>
+                                <p>Completed Trips</p>
+                            </div>
+                            <div className="about__card">
+                                <h4>176</h4>
+                                <p>Tour Guides</p>
+                            </div>
+                            <div className="about__card">
+                                <h4>153</h4>
+                                <p>Destinations</p>
+                            </div>
+                        </div>
+                        <button class="btn">
+                            Read More <i className="ri-arrow-right-line"></i>
+                        </button>
+                    </div>
+                    <div className="about__image">
+                        <img src="https://cdn.leonardo.ai/users/cc91876d-418b-4856-82a3-b277d5cda999/generations/378f4e2b-f74c-495d-8842-e7b667ec9fba/DreamShaper_v7_travel_the_world_destination_to_venture_where_n_0.jpg" alt="about" />
+                    </div>
+                </div>
+            </section>
+        </div>
+    );
 }
 
-export default Home
+export default Home;
+
+
+
+
